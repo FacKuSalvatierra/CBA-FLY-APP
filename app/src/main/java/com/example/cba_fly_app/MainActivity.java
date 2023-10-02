b76e2c4a8f60493e18571e5caa0f13099d4d4a43
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2023-10-01 22:50:57 -0300

    Added all Activities

--- app/src/main/java/com/example/cba_fly_app/MainActivity.java
+++ app/src/main/java/com/example/cba_fly_app/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.cba_fly_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText email = findViewById(R.id.loginEmail);
+        EditText password = findViewById(R.id.loginPassword);
+    }
+    public void iniciarSesion(View v) {
+        Toast.makeText(this, "¡Botón apretado!", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onStart() {
