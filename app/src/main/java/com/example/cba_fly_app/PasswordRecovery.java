2d97089165de34118c0eeeabe6d507034b28bc62
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2023-10-02 03:21:41 -0300

    Finished Booking, PasswordRecovery and RegisterAccount Activity

--- app/src/main/java/com/example/cba_fly_app/PasswordRecovery.java
+++ app/src/main/java/com/example/cba_fly_app/PasswordRecovery.java
@@ -2,7 +2,10 @@ package com.example.cba_fly_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class PasswordRecovery extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class PasswordRecovery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_recovery);
+    }
+    public void volverAtras(View v) {
+        Intent goBack=new Intent(this, MainActivity.class);
+        startActivity(goBack);
+    }
+    public void enviarCorreo(View v) {
+        Toast.makeText(this, "¡Correo enviado!", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/cba_fly_app/RegisterAccount.java
+++ app/src/main/java/com/example/cba_fly_app/RegisterAccount.java
@@ -2,7 +2,9 @@ package com.example.cba_fly_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterAccount extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class RegisterAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+    }
+
+    public void crearCuenta(View v) {
+        Intent register = new Intent(this, Profile.class);
+        startActivity(register);
+    }
+
+    public void goLogin(View v) {
+        Intent login = new Intent(this, MainActivity.class);
+        startActivity(login);
     }
 }
